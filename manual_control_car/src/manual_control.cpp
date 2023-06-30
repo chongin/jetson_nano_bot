312d15ec55dcb2270012b69ee66309a87f7a5169
chongin.ng <ngchongin@gmai.com>
2023-06-30 11:26:41 -0400

    update use SDL library to catch key press and release event

--- manual_control_car/src/manual_control.cpp
+++ manual_control_car/src/manual_control.cpp
@@ -1,53 +1,77 @@
+#include <iostream>
+#include <SDL2/SDL.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include <ncurses.h>
+#include <csignal>
 
 
-std::map<char, std::string> key_mapping = {
-    {KEY_UP, "up"},
-    {KEY_DOWN, "down"},
-    {KEY_LEFT, "left"},
-    {KEY_RIGHT, "right"},
-    {119, "speed+"}, //w
-    {115, "speed-"} //s
+std::map<char, std::string> KEY_MAPPING = {
+    {'W', "up"},
+    {'S', "down"},
+    {'A', "left"},
+    {'D', "right"},
+    {'Q', "speed+"}, //w
+    {'E', "speed-"} //s
 };
+
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "manual_control_node");
     ros::NodeHandle nh;
-
     ros::Publisher pub = nh.advertise<std_msgs::String>("drive_car", 10);
 
-    initscr();
-    cbreak();
-    noecho();
-    keypad(stdscr, TRUE);
-    nodelay(stdscr, TRUE);
+    SDL_Init(SDL_INIT_VIDEO); // Initialize SDL
 
-    refresh();
-    while (ros::ok())
-    {
-        int key = getch();
-        if (key != -1) {
-            if (key_mapping.count(key) > 0) {
-                std_msgs::String msg;
-                msg.data = key_mapping[key];
-                pub.publish(msg);
-            }
-            else if (key == 3) {
-                std::cout << "Ctrl-C exit." << std::endl; 
-                break;
-            }
-            else {
-                std_msgs::String msg;
-                msg.data = "stop";
-                pub.publish(msg);
+    SDL_Window* window = SDL_CreateWindow("Drive Car", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    bool keyDown[SDL_NUM_SCANCODES] = {false}; // Array to track key state
+    bool running = true;
+    std_msgs::String message;
+    const char* key = nullptr;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_KEYDOWN:
+                    keyDown[event.key.keysym.scancode] = true;
+
+                    key = SDL_GetScancodeName(event.key.keysym.scancode);
+                    if (KEY_MAPPING.count(*key) > 0)
+                    {
+                        message.data = KEY_MAPPING[*key];
+                        pub.publish(message);
+                    }
+
+                    std::cout << "Key Pressed: " << key << std::endl;
+                    break;
+                case SDL_KEYUP:
+                    keyDown[event.key.keysym.scancode] = false;
+
+                    key = SDL_GetScancodeName(event.key.keysym.scancode);
+                    if (KEY_MAPPING.count(*key) > 0)
+                    {
+                        message.data = "stop";
+                        pub.publish(message);
+                    }
+
+                    std::cout << "Key Released: " << SDL_GetScancodeName(event.key.keysym.scancode) << std::endl;
+                    break;
+                default:
+                    // Handle other event types
+                    break;
             }
         }
-       
-        ros::spinOnce();
     }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit(); // Cleanup SDL
 
     return 0;
 }
